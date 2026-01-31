d68996e460fb24575127a772649f34c2800c3fdf
ShivamLohia1 <contactshivamlohia@gmail.com>
2026-01-31 12:44:25 +0530

    Added Cart model

--- models/routes/cartRoutes.js
+++ models/routes/cartRoutes.js
@@ -2,23 +2,14 @@ const express = require("express");
 const router = express.Router();
 const Cart = require("../models/Cart");
 
-// GET cart items
+// GET all cart items
 router.get("/", async (req, res) => {
-  const items = await Cart.find();
-  res.json(items);
-});
-
-// ADD item to cart
-router.post("/add", async (req, res) => {
-  const item = new Cart(req.body);
-  await item.save();
-  res.json({ message: "Item added to cart" });
-});
-
-// DELETE item
-router.delete("/:id", async (req, res) => {
-  await Cart.findByIdAndDelete(req.params.id);
-  res.json({ message: "Item removed" });
+  try {
+    const items = await Cart.find();
+    res.json(items);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
