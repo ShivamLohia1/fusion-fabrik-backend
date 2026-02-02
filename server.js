1a4b29e279e9fe2ae1b7693f9aed2b6e0c7c99e0
ShivamLohia1 <contactshivamlohia@gmail.com>
2026-02-02 17:46:20 +0530

    Fixed server startup

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
@@ -12,19 +11,13 @@ app.get("/", (req, res) => {
   res.send("FusionFabrik Backend API Running 🚀");
 });
 
-
-// API Routes
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/products", require("./routes/productRoutes"));
 app.use("/api/cart", require("./routes/cartRoutes"));
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-  console.log("MongoDB Connected");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-})
-.catch(err => {
-  console.log("DB Connection Error:", err);
-});
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log("DB Error:", err));
